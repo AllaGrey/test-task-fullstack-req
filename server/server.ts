b7645b65eeb7fdab682522eef4b30e981c1cfc46
AllaGrey <111253692+AllaGrey@users.noreply.github.com>
2024-08-12 01:35:19 +0300

    update server

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,7 @@
 import app from "./app";
-import { Server } from "http";
 
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
